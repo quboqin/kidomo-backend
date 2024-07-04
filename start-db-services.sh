e154ddf41f3bbc03cbb5a40a50978348d4d149ea
Qubo Qin <quboqin0710@gmail.com>
2024-07-04 13:45:02 +0800

    add  docker-compose.db.yml

---
+++ start-db-services.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Function to stop docker-compose services
+cleanup() {
+  echo "Stopping docker-compose services..."
+  docker-compose -f docker-compose.db.yml down
+  exit 0
+}
+
+# Trap SIGINT (Ctrl-C) and call cleanup function
+trap 'cleanup' SIGINT
+
+# Check for --delete-db argument
+if [ "$1" == "--delete-db" ]; then
+  echo "Deleting db/data folder..."
+  rm -rf db/data
+fi
+
+docker-compose -f docker-compose.db.yml up -d
+
+# services=("user" "task" "mailer" "permission" "token" "gateway")
+# for service in "${services[@]}"; do
+#   echo "Now starting service: $service..."
+#   (cd "$service" && nohup npm run start:dev >> ../nohup.out &)
+# done
+
+wait
+echo "All services have been started."
+
+# Keep script running until Ctrl-C is pressed
+while true; do
+  sleep 1
+done
